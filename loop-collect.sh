7643a0b9d86c20f9fd523e0283804ebf1e9f67ba
Etienne Dublé <etienne.duble@imag.fr>
2016-03-18 15:22:42 +0100

    loop-collect.sh: check if collector is already run
    With this change, loop-collect.sh can be restarted periodically
    (e.g. every 3 minutes in the crontab). If the collector is
    already running, nothing happens. It it is not running (whatever
    is the issue) it is started.
    
    As a result, I modified the crontab as follows:
    @reboot /home/slide/duble/twitter-collector/loop-collect.sh
    */3 * * * * /home/slide/duble/twitter-collector/loop-collect.sh

--- loop-collect.sh
+++ loop-collect.sh
@@ -10,6 +10,12 @@ then
         sleep 3
     done
 else
+    ps_num=$(ps -ef | grep collector.py | grep -v grep | wc -l)
+    if [ $ps_num -gt 0 ]
+    then
+        echo 'Collector is already running.'
+        exit
+    fi
     cd $THIS_DIR
     screen -S collector -d -m ./loop-collect.sh step2
 fi
